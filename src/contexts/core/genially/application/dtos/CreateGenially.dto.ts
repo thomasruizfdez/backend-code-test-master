884edb8969d634c85a6b92098384398f1fa53359
Tomás Ruiz Fernández <tom.ir.f1@gmail.com>
2025-04-27 17:14:05 +0200

    feat: new param

--- src/contexts/core/genially/application/dtos/CreateGenially.dto.ts
+++ src/contexts/core/genially/application/dtos/CreateGenially.dto.ts
@@ -14,4 +14,9 @@ export class CreateGeniallyDto  {
     @MaxLength(125)
     @IsNotEmpty()
     description: string;
+
+    @IsString()
+    @MaxLength(125)
+    @IsNotEmpty()
+    mipolla: string;
 }
